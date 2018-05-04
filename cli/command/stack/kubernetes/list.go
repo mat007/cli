0ec79c96b6b5f80315563dc9332259ba524e213f
Mathieu Champlon <mathieu.champlon@docker.com>
2018-05-04 10:29:44 +0200

    Move Kubernetes stack retrieval to the kubernetes namespace
    Signed-off-by: Mathieu Champlon <mathieu.champlon@docker.com>

--- cli/command/stack/kubernetes/list.go
+++ cli/command/stack/kubernetes/list.go
@@ -1,13 +1,89 @@
 package kubernetes
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+
+	"github.com/docker/cli/cli/command"
 	"github.com/docker/cli/cli/command/formatter"
 	"github.com/docker/cli/cli/command/stack/options"
+	"github.com/docker/go-connections/tlsconfig"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
+	core_v1 "k8s.io/api/core/v1"
+	apierrs "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// GetStacks lists the kubernetes stacks.
-func GetStacks(kubeCli *KubeCli, opts options.List) ([]*formatter.Stack, error) {
+// GetStacks lists the kubernetes stacks
+func GetStacks(dockerCli command.Cli, opts options.List, flags *pflag.FlagSet) ([]*formatter.Stack, error) {
+	if dockerCli.ClientInfo().HasAll() && !flags.Changed("namespace") {
+		opts.AllNamespaces = true
+	}
+	if opts.AllNamespaces {
+		return getStacksWithAllNamespaces(dockerCli, opts, flags)
+	}
+	return getStacksWithNamespaces(dockerCli, opts, flags)
+}
+
+func getStacksWithAllNamespaces(dockerCli command.Cli, opts options.List, flags *pflag.FlagSet) ([]*formatter.Stack, error) {
+	stacks, err := getStacks(dockerCli, opts, NewOptions(flags))
+	if err == nil || !apierrs.IsForbidden(err) {
+		return stacks, err
+	}
+	nms, err2 := getUserVisibleNamespaces(dockerCli)
+	if err2 != nil {
+		logrus.Warnf("Failed to query user visible namespaces: %s", err2)
+		return nil, err
+	}
+	opts.AllNamespaces = false
+	for _, nm := range nms.Items {
+		ss, err := getStacks(dockerCli, opts, NewOptions(flags, nm.Name))
+		if err != nil {
+			return nil, err
+		}
+		stacks = append(stacks, ss...)
+	}
+	return stacks, nil
+}
+
+func getStacksWithNamespaces(dockerCli command.Cli, opts options.List, flags *pflag.FlagSet) ([]*formatter.Stack, error) {
+	mnms, err := getNamespaces(flags)
+	if err != nil {
+		return nil, err
+	}
+	stacks := []*formatter.Stack{}
+	for nm := range mnms {
+		ss, err := getStacks(dockerCli, opts, NewOptions(flags, nm))
+		if err != nil {
+			return nil, err
+		}
+		stacks = append(stacks, ss...)
+	}
+	return stacks, nil
+}
+
+func getNamespaces(flags *pflag.FlagSet) (map[string]struct{}, error) {
+	nms, err := flags.GetStringSlice("namespace")
+	if err != nil {
+		return nil, err
+	}
+	mnms := map[string]struct{}{}
+	for _, nm := range nms {
+		mnms[nm] = struct{}{}
+	}
+	return mnms, nil
+}
+
+func getStacks(dockerCli command.Cli, opts options.List, kopts Options) ([]*formatter.Stack, error) {
+	kubeCli, err := WrapCli(dockerCli, kopts)
+	if err != nil {
+		return nil, err
+	}
 	composeClient, err := kubeCli.composeClient()
 	if err != nil {
 		return nil, err
@@ -30,4 +106,50 @@ func GetStacks(kubeCli *KubeCli, opts options.List) ([]*formatter.Stack, error) 
 		})
 	}
 	return formattedStacks, nil
+}
+
+func getUserVisibleNamespaces(dockerCli command.Cli) (*core_v1.NamespaceList, error) {
+	host := dockerCli.Client().DaemonHost()
+	endpoint, err := url.Parse(host)
+	if err != nil {
+		return nil, err
+	}
+	endpoint.Scheme = "https"
+	endpoint.Path = "/kubernetesNamespaces"
+	res := core_v1.NamespaceList{}
+	return &res, makeRequest(dockerCli, *endpoint, func(resp http.Response) error {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return errors.Wrapf(err, "received %d status and unable to read response", resp.StatusCode)
+		}
+		if resp.StatusCode != http.StatusOK {
+			return fmt.Errorf(string(body))
+		}
+		if err := json.Unmarshal(body, &res); err != nil {
+			return errors.Wrapf(err, "unmarshal failed: %s", string(body))
+		}
+		return nil
+	})
+}
+
+func makeRequest(dockerCli command.Cli, endpoint url.URL, handler func(resp http.Response) error) error {
+	tlsOptions := dockerCli.ClientInfo().TLSOptions
+	if tlsOptions == nil {
+		return fmt.Errorf("missing TLS options for https")
+	}
+	tlsConfig, err := tlsconfig.Client(*tlsOptions)
+	if err != nil {
+		return err
+	}
+	httpClient := http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: tlsConfig,
+		},
+	}
+	resp, err := httpClient.Get(endpoint.String())
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	return handler(*resp)
 }
--- cli/command/stack/list.go
+++ cli/command/stack/list.go
@@ -1,11 +1,6 @@
 package stack
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"net/url"
 	"sort"
 
 	"github.com/docker/cli/cli"
@@ -14,12 +9,7 @@ import (
 	"github.com/docker/cli/cli/command/stack/kubernetes"
 	"github.com/docker/cli/cli/command/stack/options"
 	"github.com/docker/cli/cli/command/stack/swarm"
-	"github.com/docker/go-connections/tlsconfig"
-	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
-	core_v1 "k8s.io/api/core/v1"
-	apierrs "k8s.io/apimachinery/pkg/api/errors"
 	"vbom.ml/util/sortorder"
 )
 
@@ -57,129 +47,13 @@ func runList(cmd *cobra.Command, dockerCli command.Cli, opts options.List) error
 		stacks = append(stacks, ss...)
 	}
 	if dockerCli.ClientInfo().HasKubernetes() {
-		flags := cmd.Flags()
-		if dockerCli.ClientInfo().HasAll() && !flags.Changed("namespace") {
-			opts.AllNamespaces = true
+		ss, err := kubernetes.GetStacks(dockerCli, opts, cmd.Flags())
+		if err != nil {
+			return err
 		}
-		if opts.AllNamespaces {
-			ss, err := getStacksWithAllNamespaces(dockerCli, opts, flags)
-			if err != nil {
-				return err
-			}
-			stacks = append(stacks, ss...)
-		} else {
-			ss, err := getStacksWithNamespaces(dockerCli, opts, flags)
-			if err != nil {
-				return err
-			}
-			stacks = append(stacks, ss...)
-		}
+		stacks = append(stacks, ss...)
 	}
 	return format(dockerCli, opts, stacks)
-}
-
-func getStacksWithAllNamespaces(dockerCli command.Cli, opts options.List, flags *pflag.FlagSet) ([]*formatter.Stack, error) {
-	stacks, err := getStacks(dockerCli, opts, kubernetes.NewOptions(flags))
-	if err == nil || !apierrs.IsForbidden(err) {
-		return stacks, err
-	}
-	nms, err := getUserVisibleNamespaces(dockerCli, err)
-	if err != nil {
-		return nil, err
-	}
-	opts.AllNamespaces = false
-	for _, nm := range nms.Items {
-		ss, err := getStacks(dockerCli, opts, kubernetes.NewOptions(flags, nm.Name))
-		if err != nil {
-			return nil, err
-		}
-		stacks = append(stacks, ss...)
-	}
-	return stacks, nil
-}
-
-func getStacksWithNamespaces(dockerCli command.Cli, opts options.List, flags *pflag.FlagSet) ([]*formatter.Stack, error) {
-	mnms, err := getNamespaces(flags)
-	if err != nil {
-		return nil, err
-	}
-	stacks := []*formatter.Stack{}
-	for nm := range mnms {
-		ss, err := getStacks(dockerCli, opts, kubernetes.NewOptions(flags, nm))
-		if err != nil {
-			return nil, err
-		}
-		stacks = append(stacks, ss...)
-	}
-	return stacks, nil
-}
-
-func getStacks(dockerCli command.Cli, opts options.List, kopts kubernetes.Options) ([]*formatter.Stack, error) {
-	kli, err := kubernetes.WrapCli(dockerCli, kopts)
-	if err != nil {
-		return nil, err
-	}
-	ss, err := kubernetes.GetStacks(kli, opts)
-	if err != nil {
-		return nil, err
-	}
-	return ss, nil
-}
-
-func getNamespaces(flags *pflag.FlagSet) (map[string]struct{}, error) {
-	nms, err := flags.GetStringSlice("namespace")
-	if err != nil {
-		return nil, err
-	}
-	mnms := map[string]struct{}{}
-	for _, nm := range nms {
-		mnms[nm] = struct{}{}
-	}
-	return mnms, nil
-}
-
-func getUserVisibleNamespaces(dockerCli command.Cli, forbiddenErr error) (*core_v1.NamespaceList, error) {
-	host := dockerCli.Client().DaemonHost()
-	endpoint, err := url.Parse(host)
-	if err != nil || endpoint.Scheme != "tcp" {
-		return nil, forbiddenErr
-	}
-	tlsOptions := dockerCli.ClientInfo().TLSOptions
-	if tlsOptions == nil {
-		return nil, forbiddenErr
-	}
-	tlsConfig, err := tlsconfig.Client(*tlsOptions)
-	if err != nil {
-		return nil, err
-	}
-	httpClient := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: tlsConfig,
-		},
-	}
-	endpoint.Scheme = "https"
-	endpoint.Path = "/kubernetesNamespaces"
-	return listUserNamespaces(httpClient, *endpoint)
-}
-
-func listUserNamespaces(httpClient *http.Client, endpoint url.URL) (*core_v1.NamespaceList, error) {
-	resp, err := httpClient.Get(endpoint.String())
-	if err != nil {
-		return nil, errors.Wrap(err, "unable to get user namespaces")
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, errors.Wrapf(err, "unable to list user namespaces: received %d status and unable to read response", resp.StatusCode)
-	}
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("unable to list user namespaces: %s", string(body))
-	}
-	var res core_v1.NamespaceList
-	if err := json.Unmarshal(body, &res); err != nil {
-		return nil, errors.Wrapf(err, "unable to unmarshal user namespaces: %s", string(body))
-	}
-	return &res, nil
 }
 
 func format(dockerCli command.Cli, opts options.List, stacks []*formatter.Stack) error {
